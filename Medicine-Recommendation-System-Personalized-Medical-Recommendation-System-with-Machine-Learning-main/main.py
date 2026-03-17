d8a85a64d7a7519811f483fcc2925c675d3aae01
Ayushi Katiyar <150540733+33Ayushi@users.noreply.github.com>
2026-03-18 00:28:03 +0530

    Update main.py

--- Medicine-Recommendation-System-Personalized-Medical-Recommendation-System-with-Machine-Learning-main/main.py
+++ Medicine-Recommendation-System-Personalized-Medical-Recommendation-System-with-Machine-Learning-main/main.py
@@ -119,6 +119,8 @@ def blog():
     return render_template("blog.html")
 
 
-if __name__ == '__main__':
+import os
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
